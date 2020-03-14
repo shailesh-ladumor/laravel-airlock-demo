78cb1ad4ed6aac6d981b430e8b320f9d1e2777d1
Shailesh Ladumor <shaileshmladumor@gmail.com>
2020-03-14 19:57:26 +0530

    first commit

---
+++ app/Http/Controllers/AuthAPIController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthAPIController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        return $user->createToken($request->password)->plainTextToken;
+    }
+
+    public function logout(Request $request){
+       return $request->user()->tokens()->delete();
+    }
+}
